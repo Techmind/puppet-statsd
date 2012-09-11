7da4f138c0798e925d54db6ef8c3b731cdca683e
techmind <bogunov@gmail.com>
2012-09-11 12:22:13 +0400

    added support to manifest file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,35 @@
-class statsd ($graphite_host, $graphite_port = 2003, $port = 8125, $debug = 0, $flush_interval = 60000) {
-	package { "nodejs-stable-release":
+class statsd ($graphite_host = "localhost", $graphite_port = 2003, $port = 8125, $debug = 0, $flush_interval = 60000) {
+	$init_command = $operatingsystem ? {
+                OpenSuSE  => "/etc/init.d/statsd restart",
+                default => ""/sbin/stop statsd; /sbin/start statsd"",
+        }
+	$init_file = $operatingsystem ? {
+                OpenSuSE  => "/etc/init.d/statsd",
+                default => "/etc/init/statsd.conf",
+        }
+	$init_file_source = $operatingsystem ? {
+                OpenSuSE  => "puppet:///modules/statsd/statsd.initd.systemv.suse",
+                default => "puppet:///modules/statsd/statsd.init.upstart",
+        }
+
+	$main_package = $operatingsystem ? {
+        	OpenSuSE  => "nodejs",
+     		default => "nodejs-stable-release",
+    	}
+	$development_packages = $operatingsystem ? {            
+                OpenSuSE  => "nodejs-devel",
+                default => "nodejs-compat-symlinks",
+        }
+
+	package { $main_package:
 		ensure => present;
 	}
 	package {
-		"nodejs-compat-symlinks":
-			require => Package["nodejs-stable-release"],
+		$development_packages:
+			require => Package[$development_packages],
 			ensure => present;
 		"npm":
-			require => Package["nodejs-stable-release"],
+			require => Package[$main_package],
 			ensure => present;
 	}
 	exec { "npm-statsd":
@@ -19,25 +41,25 @@ class statsd ($graphite_host, $graphite_port = 2003, $port = 8125, $debug = 0, $
 	}
 
         file {
-		"/etc/init/statsd.conf":
+		$init_file:
                 	ensure => file,
 			owner   => "root",
 			group   => "root",
 			mode    => "0644",
-                	source => "puppet:///modules/statsd/statsd.init.upstart";
+                	source => $init_file_source;
 
 		"/etc/statsd.js":
 			ensure => file,
 			content => template("statsd/statsd.js.erb");
 	}
 	exec { "restart-statsd":
-		require => File["/etc/init/statsd.conf"],
+		require => File[$init_file],
 		subscribe  => [
-				File['/etc/init/statsd.conf'],
+				File[$init_file],
 				File['/etc/statsd.js'],
 				Exec['npm-statsd'],
-				Package['nodejs-compat-symlinks']
+				Package[$development_packages]
 			],
-		command => "/sbin/stop statsd; /sbin/start statsd";
+		command => $init_command;
 	}
 }
